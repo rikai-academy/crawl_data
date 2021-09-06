1d04c70ab2df458f2f0feffc3162b277226208e1
tranphuongnam <phuongnamgroupers@gmail.com>
2021-09-06 16:11:38 +0700

    crawl data web imitsu

---
+++ main.rb
@@ -0,0 +1,5 @@
+require './handleCrawler/handle_craw.rb'
+objHandleCrawler = HandleCrawler.new
+
+# Call function startCrawler of class HandleCrawler to start crawling
+objHandleCrawler.startCrawler()
